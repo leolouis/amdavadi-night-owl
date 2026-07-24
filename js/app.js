3ff354c9357b32c95bf55c6a58ab0d8c7b512a30
Leo Louis <leo.louis@yahoo.com>
2026-07-24 10:33:02 +0530

    Add nightMap and locateUser functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-import { initMap } from "./map.js";
+import { initMap, nightMap } from "./map.js";
+import { locateUser } from "./geolocation.js";
+
 
 window.onload = async () => {
 
+
     await initMap();
 
-    document.getElementById("loading-screen").style.display = "none";
+
+    document
+    .getElementById("loading-screen")
+    .style.display = "none";
+
+
+
+    document
+    .getElementById("locateBtn")
+    .addEventListener(
+        "click",
+        ()=>locateUser(nightMap)
+    );
+
 
 }
