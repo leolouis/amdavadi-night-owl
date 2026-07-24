69d0325d39b582cbac6bc5b1a66f89fd9511cee1
Leo Louis <leo.louis@yahoo.com>
2026-07-24 10:08:25 +0530

    Add configuration file with default settings

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+    githubRepo: "leolouis/amdavadi-night-owl",
+
+    placesFile: "places.json",
+
+    defaultLocation: {
+
+        lat: 23.0225,
+
+        lng: 72.5714,
+
+        zoom: 13
+
+    }
+
+}
